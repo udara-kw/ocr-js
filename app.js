8281dff9af34b6546f94122e8c0c660bc2d1a334
Udara Weerasinghe <64691719+udara-kw@users.noreply.github.com>
2021-05-11 18:17:28 +0530

    Complete basic implementation

--- app.js
+++ app.js
@@ -8,27 +8,53 @@ const fs = require("fs");
 const multer = require("multer");
 
 //read images
-const createWorker = require("tesseract.js");
-const { createBrotliCompress } = require("zlib");
+const { createWorker, RecognizeResult } = require("tesseract.js");
+
+const worker = createWorker({
+  logger: (m) => console.log(m),
+});
 
 //save images
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, "./uploads");
   },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
-const upload = multer({ strage: storage }).single("avatar");
+const upload = multer({ storage: storage }).single("avatar");
 
 app.set("view engine", "ejs");
+
+//add public folder (for styling)
+app.use(express.static("public"));
 
 //routes
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
+
 app.post("/upload", (req, res) => {
   upload(req, res, (err) => {
-    console.log(req.file);
+    fs.readFile(`./uploads/${req.file.originalname}`, async (err, img) => {
+      if (err) return console.log("Ths is your error", err);
+      await worker.load();
+      await worker.loadLanguage("eng");
+      await worker.initialize("eng");
+      await worker.recognize(img);
+      res.redirect("/download");
+      const { data } = await worker.getPDF("Tesseract OCR Result");
+      fs.writeFileSync("tesseract-ocr-result.pdf", Buffer.from(data));
+      console.log("Generate PDF: tesseract-ocr-result.pdf");
+      await worker.terminate();
+    });
   });
+});
+
+app.get("/download", (req, res) => {
+  const file = `${__dirname}/tesseract-ocr-result.pdf`;
+  res.download(file);
 });
 
 //Start up server
